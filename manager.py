23d733b4d44a723fb83aeb9338c205ff4cc5bef8
Joseph <josephodr@hotmail.com>
2017-12-03 23:09:51 +0000

    Manager now gets list of urls from commit page to send to workers

--- manager.py
+++ manager.py
@@ -3,39 +3,59 @@ import requests
 from collections import deque
 import shutil
 
-def get_github_file():
-	commits_list = []
+def get_sha_list(github_url):
+	sha_list = []
 
-	resp = requests.get(
-		'https://api.github.com/repos/JTODR/Chat-Server-Project/commits')
+	token = get_token()
+	payload = {'access_token': token}
+	headers = {'Accept': 'application/vnd.github.v3.raw'}
+	resp = requests.get(github_url,   params=payload, headers=headers)
 
 	for item in resp.json():
-		commits_list.append(item['sha'])
-		#print(item)
+		sha_list.append(item['sha'])
 
-	#print(len(commits_list))
-	#print(commits_list)
-	#print(commits_list[0])
+	return sha_list
 
+def get_raw_url_list(github_url, sha_list):
 
-	resp = requests.get(
-		'https://api.github.com/repos/JTODR/Chat-Server-Project/commits/' + commits_list[0])
-	print(resp.json()['files'])
-	files = {}
-	files = resp.json()['files']
-	file_dict = files[0]
+	raw_url_list = []
+	token = get_token()
+	payload = {'access_token': token}
+	headers = {'Accept': 'application/vnd.github.v3.raw'}
+	for sha in sha_list:
+		resp = requests.get(github_url + '/' + sha,  params=payload, headers=headers)
+		#print(resp.json()['files'])
+		files = {}
+		files = resp.json()['files']
+		print(files)
+		print()
+		if len(files) != 0:
+			file_dict = files[0]
+			raw_url_list.append(file_dict['raw_url'])
+	return raw_url_list
 
-	blob_url = file_dict['raw_url']
-	print(blob_url)
+	#print(raw_url)
 	
-	resp = requests.get(blob_url)
-	with open('filetest.txt', 'w') as tmp_file:
-				tmp_file.write(resp.text)
+	#resp = requests.get(raw_url)
+	#print(resp.text)
+	#with open('filetest.txt', 'w') as tmp_file:
+	#			tmp_file.write(resp.text)
 
+def get_token():
+	with open('github-token.txt', 'r') as tmp_file:
+		token = tmp_file.read()
+
+	#token = 'access_token '+token
+	print(token)
+	return token
 
 def main():
-	
-	get_github_file()
+
+	github_url = 'https://api.github.com/repos/JTODR/Chat-Server-Project/commits'
+
+	sha_list = get_sha_list(github_url)
+	raw_url_list = get_raw_url_list(github_url, sha_list)
+	print (raw_url_list)
 	'''serverName = 'localhost'
 	serverPort = 1234  
 	
