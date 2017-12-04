217131af93a17efdefcbb91e10d019402f885954
Joseph <josephodr@hotmail.com>
2017-12-04 18:05:26 +0000

    Worker now returns CC of each file in each tree of all commits

--- manager.py
+++ manager.py
@@ -4,127 +4,92 @@ from collections import deque
 import shutil
 
 def get_tree_urls(github_url):
-	
+    
 
-	tree_urls = []
+    tree_urls = []
 
-	payload_headers = get__params_headers()
+    payload_headers = get__params_headers()
 
-	resp = requests.get(github_url,   params=payload_headers[0], headers=payload_headers[1])	# get the commit page of the github url
+    resp = requests.get(github_url,   params=payload_headers[0], headers=payload_headers[1])    # get the commit page of the github url
 
-	for item in resp.json():
-		tree_urls.append(item['commit']['tree']['url'])	# parse out the SHA's from the commit page
+    for item in resp.json():
+        tree_urls.append(item['commit']['tree']['url']) # parse out the tree URL's from the commit page
 
-	return tree_urls
+    return tree_urls
 
 
 def get_blob_url_list(github_url, tree_urls):
-	#count = 0
-	blob_url_list = []
-	payload_headers = get__params_headers()	
-	for blob_url in tree_urls:
-		resp = requests.get(blob_url,   params=payload_headers[0], headers=payload_headers[1])
 
-		tree = resp.json()['tree']
+    blob_url_list = []
+    payload_headers = get__params_headers() 
+    for blob_url in tree_urls:
+        resp = requests.get(blob_url,   params=payload_headers[0], headers=payload_headers[1])
 
-		for item in tree:
-			file_url = item['url']
-			filename = item['path']
+        tree = resp.json()['tree']
 
-			url_filename = file_url + '|' + filename
-			blob_url_list.append(url_filename)
+        for item in tree:
+            file_url = item['url']
+            filename = item['path']
 
-	return blob_url_list
+            url_filename = file_url + '|' + filename
+            blob_url_list.append(url_filename)
 
-			#flag = check_py(filename)
-			#if flag == True:
-
-			#	resp = requests.get(file_url,   params=payload_headers[0], headers=payload_headers[1])
-
-			#	filename = str(count)
-			#	file_path = ".\\temp\\" + filename + '.py'  
-
-			#	with open(file_path, 'w') as tmp_file:
-			#		tmp_file.write(resp.text)
-			#	tmp_file.close()
-			#	count +=1
-		#if count == 15:
-			#break
-
-
-'''
-	raw_url_list = []
-
-	payload_headers = get__params_headers()
-
-	for tree in tree_urls:
-		resp = requests.get(github_url + '/' + tree,  params=payload_headers[0], headers=payload_headers[1])		# get the individual commit from the current SHA
-		files = {}
-		files = resp.json()['files']	# parse out the files from that commit
-
-		if len(files) != 0:
-			file_dict = files[0]
-			raw_url_list.append(file_dict['raw_url'])	# parse out the raw URL of the file 
-	return raw_url_list
-'''
-
+    return blob_url_list
 
 def get__params_headers():
-	with open('github-token.txt', 'r') as tmp_file:
-		token = tmp_file.read()		# get the token from a text file in current directory
+    with open('github-token.txt', 'r') as tmp_file:
+        token = tmp_file.read()     # get the token from a text file in current directory
 
-	payload = {'access_token': token}
-	headers = {'Accept': 'application/vnd.github.v3.raw'}
+    payload = {'access_token': token}
+    headers = {'Accept': 'application/vnd.github.v3.raw'}
 
-	#print(token)
-	return (payload, headers)
+    #print(token)
+    return (payload, headers)
 
-def send_work(raw_url_list):
-	serverName = 'localhost'
-	serverPort = 1598
-	total_cc = 0
+def send_work(blob_url_list):
+    serverName = 'localhost'
+    serverPort = 1598
+    total_cc = 0
 
-	count = 0
+    count = 0
 
-	# send URL's one by one to the worker
-	for raw_url in raw_url_list:	
-		manager_socket = socket(AF_INET, SOCK_STREAM)
-		manager_socket.connect((serverName,serverPort))
-		manager_socket.send(raw_url.encode())		
-		print("SENT: " + raw_url)
+    # send URL's one by one to the worker
+    for blob_url in blob_url_list:  
+        manager_socket = socket(AF_INET, SOCK_STREAM)
+        manager_socket.connect((serverName,serverPort))
+        manager_socket.send(blob_url.encode())      
+        print("SENT: " + blob_url)
 
-		reply = manager_socket.recv(1024)	# receive the cyclomatic complexity for the file at the current raw URL
-		reply = reply.decode()
-		print("RECEIVED: " + reply)
+        reply = manager_socket.recv(1024)   # receive the cyclomatic complexity for the file at the current raw URL
+        reply = reply.decode()
+        print("RECEIVED: " + reply)
 
-		if reply != "-1" and reply is not None:
-			reply = int(reply)
-			total_cc += reply 		# increment total commit CC with the reply from the worker
-			count += 1
-		manager_socket.close()
-	print ("Count = " + str(count))
-	return total_cc
+        if reply != "-1" and reply is not None:
+            reply = int(reply)
+            total_cc += reply       # increment total commit CC with the reply from the worker
+            count += 1
+        manager_socket.close()
+    print ("Count = " + str(count))
+    return total_cc
 
 def main():
 
-	#github_url = 'https://api.github.com/repos/JTODR/Distributed-File-System-Project/commits'
-	github_url = 'https://api.github.com/repos/geekcomputers/Python/commits'
+    github_url = 'https://api.github.com/repos/JTODR/Cyclomatic-Complexity-Calculator/commits'
+   
+    print("Getting SHA list...")
+    tree_urls = get_tree_urls(github_url)      # get the list of tree URL's from the project's commits
+    print("SHA list received...")
+    print("Gettng raw URL's...")
+    blob_url_list = get_blob_url_list(github_url, tree_urls)    # get blob URLs of each tree's 
+    #print (raw_url_list)
+    #blob_url_list = ['https://api.github.com/repos/geekcomputers/Python/git/blobs/b92c525955a1a4ef80cfad2564db957d78716996|4 Digit Number Combinations.py', 'https://api.github.com/repos/geekcomputers/Python/git/blobs/90ba62afc956f576c5832d571a4c85188d817fac|CountMillionCharacter.py', 'https://api.github.com/repos/geekcomputers/Python/git/blobs/47482ba68585483023372e3999e1927ac25c7991|CountMillionCharacters-2.0.py', 'https://api.github.com/repos/geekcomputers/Python/git/trees/273f0bab9e233ec1769be11c3125cecc15c7af93|CountMillionCharacters-Variations', 'https://api.github.com/repos/geekcomputers/Python/git/blobs/5d8c91b96191295eeb110c1dad5ecdc0514bad98|Cricket_score.py', 'https://api.github.com/repos/geekcomputers/Python/git/blobs/dbdc0aa1c88b1c67359abe18c362fb787729ab26|EncryptionTool.py', 'https://api.github.com/repos/geekcomputers/Python/git/trees/7883cb00218da1977239da0ef38c624511bc1c86|Google Image Downloader', 'https://api.github.com/repos/geekcomputers/Python/git/blobs/35fed53df4c43da9e63df6632ccac45c5ace72ee|Google_News.py', 'https://api.github.com/repos/geekcomputers/Python/git/blobs/586686d066848def5d273599180e54f23cca2170|GroupSms_Way2.py']
+    print("Commenced sending of raw URL's to worker...")
+    total_cc = send_work(blob_url_list)     # send the raw URL's to the worker
 
-	print("Getting SHA list...")
-	tree_urls = get_tree_urls(github_url)		# get the list of SHA's from the project's commits
-	print("SHA list received...")
-	print("Gettng raw URL's...")
-	raw_url_list = get_blob_url_list(github_url, tree_urls)	# get raw URLs of the SHA's 
-	print (raw_url_list)
-	#raw_url_list = ['https://github.com/JTODR/Distributed-File-System-Project/raw/8386c342e144fd1e46ccea5e107112a36994a08e/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/7200a2e0c272f23f599f89afd5c801a64a80792e/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/af01ee7a4937fe332a040c371ad680c1eb849f73/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/f552ce7a2885abd76cd320641a5a8cfddd20fe00/client_lib.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/86dd0696922c6c8813a50b6953519bd57bf023dd/client_lib.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/ee6fdda9209ed1df254a00dcfb368fbcd28fedfd/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/0c803a0e386ad1fdcb02a7ada15b6231cfdf8b70/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/d4fa0dd716538033ba8ab05b8e0bdbe3080749e6/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/a4e3fc9278d44af8c5eef3a0c34cbbd9db64ad84/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/da7fc3d0316550b62fb05a6d8281a5dd74f1099d/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/f55911846ca73ea36c7ea16c0f307aaaf99552e2/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/5a3688855b908c70b625a2098085a8a7b8575f9f/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/9b5fa2b03a98a34b9e22665fd438b3f9435d8250/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/bb04b5d0569fcd05cf2527e0d0bd44011b2595cf/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/c8ad41520096c507eacad4ee419b85eb2094ad38/client.py', 'https://github.com/JTODR/Distributed-File-System-Project/raw/12b54f9da4fbe9e5077558eb9498a4406c171ae5/.gitignore', 'https://github.com/JTODR/Distributed-File-System-Project/raw/8c88429a266d63d7ad8db84c2722e1caf4c31ab2/.gitignore', 'https://github.com/JTODR/Distributed-File-System-Project/raw/c99def793705b300d4dbc7afeeb0c9de474fb63f/client.py']
+    print("Total CC is: " + str(total_cc))  
 
-	print("Commenced sending of raw URL's to worker...")
-	#total_cc = send_work(raw_url_list)		# send the raw URL's to the worker
-
-	#print("Total CC is: " + str(total_cc))	
-
-	#avg_cc = total_cc/len(raw_url_list)
-	#print("Average CC is: " + str(avg_cc))
+    avg_cc = total_cc/len(blob_url_list)
+    print("Average CC is: " + str(avg_cc))
 
 if __name__ == "__main__":
-	main()
+    main()
--- worker.py
+++ worker.py
@@ -9,83 +9,103 @@ import os.path
 import sys
 import shutil
 
+from re import match
 
 cc_config = Config(
-		exclude='',
-		ignore='venv',
-		order=SCORE,
-		no_assert=True,
-		show_closures=False,
-		min='A',
-		max='F',
+        exclude='',
+        ignore='venv',
+        order=SCORE,
+        no_assert=True,
+        show_closures=False,
+        min='A',
+        max='F',
 )
 
 
 def get_filename(raw_url):
 
-	sha_filename = raw_url.split('raw')[1]
-	filename = sha_filename.split('/')[2]
-	return filename
+    sha_filename = raw_url.split('raw')[1]
+    filename = sha_filename.split('/')[2]
+    return filename
+
+def get__params_headers():
+    with open('github-token.txt', 'r') as tmp_file:
+        token = tmp_file.read()     # get the token from a text file in current directory
+
+    payload = {'access_token': token}
+    headers = {'Accept': 'application/vnd.github.v3.raw'}
+
+    #print(token)
+    return (payload, headers)
+
+
+def check_py(filename):
+    return True if match('.*\.py', filename) is not None else False
 
 def calc_CC(raw_url, cc_config):
 
-	filename = get_filename(raw_url)
-	if filename.split('.')[1] != 'py':
-		return -1
+    blob_url = raw_url.split('|')[0]
+    filename = raw_url.split('|')[1]
+
+    payload_headers = get__params_headers()
+
+    flag = check_py(filename)
+    if flag == True:
+
+        resp = requests.get(blob_url,   params=payload_headers[0], headers=payload_headers[1])
+
+        file_path = filename
+
+        with open(file_path, 'w') as tmp_file:
+            tmp_file.write(resp.text)
+        tmp_file.close()
 
 
-	resp = requests.get(raw_url)
+        CC_file_get = open(file_path, 'r')
+        results = CCHarvester(file_path, cc_config).gobble(CC_file_get)
+        CC_file_get.close()
+        os.remove(file_path)
 
-	filename = strftime("%Y%m%d%H%M%S", gmtime()) + '.py'
+        file_cc = 0
 
-	file_path = filename    
-	with open(file_path, 'w') as tmp_file:
-		tmp_file.write(resp.text)
-	tmp_file.close()
+        for i in results:
+            print (i.complexity)
+            file_cc += int(i.complexity)
 
-	CC_file_get = open(file_path, 'r')
-	results = CCHarvester(file_path, cc_config).gobble(CC_file_get)
-	CC_file_get.close()
-	os.remove(file_path)
-
-	file_cc = 0
-
-	for i in results:
-		print (i.complexity)
-		file_cc += int(i.complexity)
-
-	#avg_cc = file_cc/ len(results)
-	print("Complexity of file: " + str(file_cc))
-	#print("Average complexity of file: " + str(avg_cc))
-	return file_cc
+        #avg_cc = file_cc/ len(results)
+        print("Complexity of file: " + str(file_cc))
+        #print("Average complexity of file: " + str(avg_cc))
+        return file_cc
+    else:
+        return -1
 
 def receive_work():
 
-	CC_socket = socket(AF_INET, SOCK_STREAM)
-	serverName = 'localhost'
-	serverPort = 1598  
-	CC_socket.bind(('', serverPort))
-	CC_socket.listen(1)
+    CC_socket = socket(AF_INET, SOCK_STREAM)
+    serverName = 'localhost'
+    serverPort = 1598  
+    CC_socket.bind(('', serverPort))
+    CC_socket.listen(1)
 
-	while True:
-		connectionSocket, addr = CC_socket.accept()
-		raw_url = connectionSocket.recv(1024)
-		raw_url = raw_url.decode()
+    while True:
+        connectionSocket, addr = CC_socket.accept()
+        raw_url = connectionSocket.recv(1024)
+        raw_url = raw_url.decode()
 
-		print("RECEIVED: " + raw_url)
+        print("RECEIVED: " + raw_url)
 
-		file_cc = calc_CC(raw_url, cc_config)
-		
-		file_cc = str(file_cc)
-		connectionSocket.send(file_cc.encode())
+        file_cc = calc_CC(raw_url, cc_config)
+        
+        file_cc = str(file_cc)
+        connectionSocket.send(file_cc.encode())
 
-	connectionSocket.close()
+    connectionSocket.close()
 
 
 def main():
 
-	print("Worker is ready to receive...")
-	receive_work()
+    print("Worker is ready to receive...")
+    receive_work()
 
 if __name__ == "__main__":
-	main()
+    main()
